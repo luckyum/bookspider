bee1f15507be73853b38fc1e6bef0042ddc5e166
祥云 <dskra@hotmail.com>
2017-12-08 14:18:25 +0800

    解决kindle格式问题

--- 23txt.py
+++ 23txt.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 #encoding: utf-8
 #description:
-#笔趣看读书网站小说抓取器 http://www.biqukan.com/
+#天籁读书网站小说抓取器 http://www.23txt.com/
 #功能：
 #   1、整理抓取文字格式
 #   2、排除重复章节
-#   3、kindl优化内容   可以通过email发送到设备浏览
 
 import urllib
 import bs4
@@ -13,8 +12,8 @@ import re
 
 
 #  小说信息配置
-story_name = "我是大玩家"
-target_url = "http://www.biqukan.com/26_26459/"
+story_name = "大王饶命"
+target_url = "http://www.23txt.com/files/article/html/44/44114/"
 
 #读取小说内容
 def readContent(download_url):
@@ -29,20 +28,10 @@ def readContent(download_url):
     soup_texts = bs4.BeautifulSoup(download_html,'lxml')
 
     #内容
-    texts = soup_texts.find_all(id = 'content', class_ = 'showtxt')
+    texts = soup_texts.find_all(id = 'nr1')
     soup_text = bs4.BeautifulSoup(str(texts), 'lxml')
     content = soup_text.div.text
-
     content = content.replace('\xa0' , '')
-    #过滤多余广告
-    content = content.replace('手机用户请浏览阅读，更优质的阅读体验。' , '')
-    content = content.replace('请记住本书首发域名：www.biqukan.com。' , '')
-    content = content.replace('笔趣阁手机版阅读网址：m.biqukan.com' , '')
-
-    #针对Kindle读取方式优化段落
-    content = content.replace('。' , '。\r\n')
-    # content = content.replace('？' , '？\r')
-    # content = content.replace('！' , '！\r')
 
     return content
 
@@ -56,7 +45,7 @@ target_response = urllib.request.urlopen(target_req)
 target_html = target_response.read().decode('gbk','utf8')
 
 listman_soup = bs4.BeautifulSoup(target_html, 'lxml')
-chapters = listman_soup.find_all('div',class_ = 'listmain')
+chapters = listman_soup.find_all('div',class_ = 'box_con')
 download_soup = bs4.BeautifulSoup(str(chapters), 'lxml')
 
 begin_flag = False
@@ -71,12 +60,12 @@ for child in download_soup.dl.children:
         continue
 
     #不知道为什么正则表达式不包含《》匹配不到内容
-    if re.match(r"《\w*》正文卷", child.string):
+    if re.match(r"《\w*》正文", child.string):
         begin_flag = True
 
     if begin_flag == True and child.a !=  None:
         download_name = child.string
-        download_url = "http://www.biqukan.com/" + child.a['href']
+        download_url = "http://m.23txt.com" + child.a['href']
         if re.match(r'\d+、\w+' , download_name):
             #排除掉重复章节
             #有连续重复，有隔章重复
@@ -85,21 +74,21 @@ for child in download_soup.dl.children:
                 num_cout += 1
                 pre_download_names[download_name] = download_url
 
-for key in pre_download_names:
-    print(key + " => " + pre_download_names[key])
+# for key in pre_download_names:
+#     print(key + " => " + pre_download_names[key])
 
-
-print("%s下载并保存文件...." % story_name)
-file = open(story_name + '.txt', 'w', encoding='utf-8')
 
 #下载每章内容
 for key in pre_download_names:
+    download_content = ""
     download_content = readContent(pre_download_names[key])
-    file.write(key + "\r\n")
-    file.write(download_content + "\r\n")
+    story = story + key + "\n"
+    story = story + download_content + "\n"
     print("已下载：( %d / %d ) " % (index, num_cout) + "\r")
     index += 1
 
-
+print("%s下载完毕，开始保存文件...." % story_name)
+file = open(story_name + '.txt', 'w', encoding='utf-8')
+file.write(story)
 file.close()
 print("文件保存完毕 => %s.txt." % story_name)
